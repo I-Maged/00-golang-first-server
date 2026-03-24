d6e140d5f722265f9d02fd2f762ed73c573fa96e
I-Maged <magedhanafy@gmail.com>
2026-03-24 19:57:18 +0200

    parsing a template

--- main.go
+++ main.go
@@ -2,28 +2,32 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 const portNumber = ":8080"
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	result := addValue(300, 4)
-	fmt.Fprintf(w, "Home: Added value = %d\n", result)
+	renderTemplate(w, "home.page.html")
 }
 func About(w http.ResponseWriter, r *http.Request) {
-	result := addValue(2, 4)
-	fmt.Fprintf(w, "About: Added value = %d\n", result)
+	renderTemplate(w, "about.page.html")
 }
 
-func addValue(x, y int) int {
-	return x + y
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("Error parsing template", err)
+		return
+	}
 }
 
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
 
-	fmt.Printf("Starting server at port %s\n", portNumber)
+	fmt.Printf("Starting server on port %s", portNumber)
 	_ = http.ListenAndServe(portNumber, nil)
 }
