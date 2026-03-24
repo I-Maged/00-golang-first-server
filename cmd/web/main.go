6661938bbf4d18f2054c2c41e69cb0b5c027f742
I-Maged <magedhanafy@gmail.com>
2026-03-24 23:09:23 +0200

    enabling go modules

---
+++ cmd/web/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/I-Maged/00-golang-first-server/pkg/handlers"
+)
+
+const portNumber = ":8080"
+
+func main() {
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
+
+	fmt.Printf("Starting server on port %s", portNumber)
+	_ = http.ListenAndServe(portNumber, nil)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/I-Maged/00-golang-first-server
+
+go 1.26.1
